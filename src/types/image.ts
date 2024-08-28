0ef7693796cf0bc57195058dc111802bd8a0c801
Sergio Martinez <elsamplio@msn.com>
2024-08-27 19:24:09 -0500

    Fixes
    Fixed http code for login and added missing attributes to types

--- src/types/image.ts
+++ src/types/image.ts
@@ -1,4 +1,5 @@
 export type Image = {
     sid: string,
-    url: string
+    url: string,
+    uploadedBy: string
 }
--- src/types/user.ts
+++ src/types/user.ts
@@ -4,4 +4,5 @@ export type User = {
   userName: string,
   password: string,
   company: string,
+  isAdmin: true,
 };
